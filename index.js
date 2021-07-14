8cd6b5eb6814d2aff451f986272ab45e71fd966b
Aditya <adityajidhonde@gmail.com>
2021-07-14 22:25:34 +0530

    My Regular Work

---
+++ index.js
@@ -0,0 +1,66 @@
+let sliderImages = document.querySelectorAll(".slide"),
+  arrowLeft = document.querySelector("#arrow-left"),
+  arrowRight = document.querySelector("#arrow-right"),
+  current = 0;
+
+// Clear all images
+function reset() {
+  for (let i = 0; i < sliderImages.length; i++) {
+    sliderImages[i].style.display = "none";
+  }
+}
+
+// Init slider
+function startSlide() {
+  reset();
+  sliderImages[0].style.display = "block";
+}
+
+// Show prev
+function slideLeft() {
+  reset();
+  sliderImages[current - 1].style.display = "block";
+  current--;
+}
+
+// Show next
+function slideRight() {
+  reset();
+  sliderImages[current + 1].style.display = "block";
+  current++;
+}
+
+// Left arrow click
+arrowLeft.addEventListener("click", function() {
+  if (current === 0) {
+    current = sliderImages.length;
+  }
+  slideLeft();
+});
+
+// Right arrow click
+arrowRight.addEventListener("click", function() {
+  if (current === sliderImages.length - 1) {
+    current = -1;
+  }
+  slideRight();
+});
+
+function funa(value,obj){
+    reset();
+    if(value=='aa'){
+        current=0;
+    }
+    else if(value=='bb'){
+        current=1;
+    }
+    else if(value=='cc'){
+        current=2;
+    }
+    else if(value=='dd'){
+        current=3;
+    }
+    sliderImages[current].style.display = "block";
+  }
+
+startSlide();
